78357ad94c6103a25eae529ed1bf986396de3401
Evan Warner <evanwarner@Evans-MacBook-Air.local>
2024-02-20 11:27:57 -0500

    Added character recognizer saving and loading

--- CharacterRecognizer.py
+++ CharacterRecognizer.py
@@ -14,15 +14,19 @@ from keras.layers import Conv2D, MaxPooling2D
 class CharacterRecognizer(object):
 
 
-    def __init__(self, model_path=None, encodings=None, input_shape=(64, 64), num_classes=62):
-        self.model = self.load_model_path(model_path)
-        self.encodings = self.encodings
-        self.input_shape = self.input_shape
-        self.num_classes = self.num_classes
+    def __init__(self, model_path=None, encodings=None, input_shape=(64, 64, 3), num_classes=62, conv_architecture=[], dense_architecture=[], epochs=25, batch_size=64):
+        self.model = self.load_model(model_path)
+        self.encodings = encodings
+        self.input_shape = input_shape
+        self.num_classes = num_classes
         self.X_train = None
         self.y_train = None
         self.X_val = None
         self.y_val = None
+        self.conv_architecture = conv_architecture
+        self.dense_architecure = dense_architecture
+        self.epochs = epochs
+        self.batch_size = batch_size
     
 
     def load_dataset(self, dataset_path, test_size=0.1):
@@ -39,7 +43,7 @@ class CharacterRecognizer(object):
                 # read the class label and load the image with cv2
                 img_path, label = row
                 img = cv2.imread(os.path.join(dataset_path, img_path))
-                resized_img = cv2.resize(img, self.input_shape, interpolation=cv2.INTER_AREA)
+                resized_img = cv2.resize(img, self.input_shape[:2], interpolation=cv2.INTER_AREA)
                 X_train.append(resized_img)
 
                 # convert the class label to one hot encoding
@@ -61,18 +65,18 @@ class CharacterRecognizer(object):
         self.encodings = one_hot_encoding
 
     
-    def build_model(self, conv_architecture, dense_architecture):
+    def build_model(self):
         # builds a CNN with the specified architecture
         model = Sequential()
 
         # add convolutional architecture
-        for num_filters in conv_architecture:
+        for num_filters in self.conv_architecture:
             model.add(Conv2D(num_filters, (3, 3), padding='same', activation='relu', input_shape=self.input_shape))
             model.add(MaxPooling2D(pool_size=(2, 2)))
         
         # flatten and add dense layers
         model.add(Flatten())
-        for width in dense_architecture:
+        for width in self.dense_architecture:
             model.add(Dense(width, activation='relu'))
         
         # add output layer
@@ -87,7 +91,7 @@ class CharacterRecognizer(object):
         self.model = model
     
 
-    def test_hyperparameters(num_conv_layers, min_conv_size, conv_increasing, num_dense_layers, min_dense_size, dense_increasing, num_epochs, batch_size):
+    def test_hyperparameters(self, num_conv_layers, min_conv_size, conv_increasing, num_dense_layers, min_dense_size, dense_increasing, num_epochs, batch_size):
         # computes the validation accuracy of the model trained with the specified hyperparameters
         num_classes = self.num_classes
         input_shape = self.X_train[0].shape
@@ -103,16 +107,20 @@ class CharacterRecognizer(object):
             dense_architecture.reverse()
 
         # build the model
-        cnn = build_cnn(input_shape, conv_architecture, dense_architecture, num_classes)
+        self.conv_architecture = conv_architecture
+        self.dense_architecture = dense_architecture
+        self.build_model()
 
         # train the model
-        train_result = cnn.fit(
-            X_train,
-            y_train,
-            validation_data=(X_val, y_val),
+        early_stopper = keras.callbacks.EarlyStopping(monitor="val_loss", patience=2)
+        train_result = self.model.fit(
+            self.X_train,
+            self.y_train,
+            validation_data=(self.X_val, self.y_val),
             batch_size=int(batch_size),
             epochs=int(num_epochs),
-            verbose=20
+            verbose=1,
+            callbacks=early_stopper
         )
 
         # return the validation accuracy
@@ -135,7 +143,7 @@ class CharacterRecognizer(object):
 
         # initialize the optimizer
         optimizer = BayesianOptimization(
-            f=test_model,
+            f=self.test_hyperparameters,
             pbounds=pbounds,
             verbose=2,
             random_state=1,
@@ -144,13 +152,26 @@ class CharacterRecognizer(object):
         # maximize the validation accuracy
         optimizer.maximize(
             init_points=2,
-            n_iter=30,
+            n_iter=1,
         )
 
+        print(optimizer)
+    
 
+    def save_model(self, save_path="character_recognizer_model.h5"):
+        # saves the model weights to the specified save path
+        self.model.save_weights(save_path)
+    
 
-
-
+    def load_model(self, model_path):
+        # loads the model weights from the specified model path
+        if model_path is None:
+            return None
+        else:
+            # build the model and load the weights
+            self.build_model()
+            self.model.load_weights(model_path)
+        
 
 if __name__ == "__main__":
     # initialize a CharacterRecognizer
@@ -158,6 +179,7 @@ if __name__ == "__main__":
 
     # load data
     cr.load_dataset("character_dataset")
+    print("Loaded Data!")
 
     # find optimal hyperparemeters
     cr.compute_optimal_hyperparamenters()
